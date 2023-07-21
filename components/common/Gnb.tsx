f6efddf7df7d25b9c34dc7250453d98c2186e5e9
dydfuf <88dydfuf@naver.com>
2023-07-21 22:34:29 +0900

    feat: gnb, bottom navigation 디자인 추가

--- components/common/Gnb.tsx
+++ components/common/Gnb.tsx
@@ -1,3 +1,4 @@
+import { ArrowLeftIcon, PlusCircledIcon } from "@radix-ui/react-icons";
 import { useRouter } from "next/router";
 
 export default function Gnb() {
@@ -11,31 +12,52 @@ export default function Gnb() {
     if (router.pathname === "/memory") {
       router.push("/memory/add");
     }
+    if (router.pathname === "/schedule") {
+      router.push("/schedule/add");
+    }
     if (router.pathname === "/plan") {
       router.push("/plan/add");
     }
-    if (router.pathname === "/schedule") {
-      router.push("/schedule/add");
+  };
+
+  const getTitle = (pathname: string) => {
+    switch (pathname) {
+      case "/memory":
+        return "추억";
+      case "/memory/add":
+        return "추억 추가하기";
+      case "/schedule":
+        return "일정";
+      case "/schedule/add":
+        return "일정 추가하기";
+      case "/plan":
+        return "계획";
+      case "/plan/add":
+        return "계획 추가하기";
+      case "/settings":
+        return "설정";
+      default:
+        return "쿠키젤리";
     }
   };
 
   return (
-    <header className="flex fixed top-0 h-40 w-full items-center px-12">
+    <header className="flex fixed top-0 h-60 w-full items-center px-12 border-b-1">
       <button
         className="mr-auto w-20 h-20 flex items-center justify-center"
         onClick={() => router.back()}
       >
-        {`<-`}
+        <ArrowLeftIcon width={30} height={30} />
       </button>
-      <div className="absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2">
-        hello world!
+      <div className="absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 text-18 tracking-widest font-semibold">
+        {getTitle(router.pathname)}
       </div>
       {showAddButton && (
         <button
           onClick={handleAddButtonClick}
           className="ml-auto w-32 h-32 flex items-center justify-center"
         >
-          Add
+          <PlusCircledIcon width={25} height={25} />
         </button>
       )}
     </header>
--- components/common/Layout.tsx
+++ components/common/Layout.tsx
@@ -1,14 +1,19 @@
+import { Poppins } from "next/font/google";
 import { PropsWithChildren } from "react";
+import { cn } from "utils/common";
 import BottomNavigation from "./BottomNavigation";
 import Gnb from "./Gnb";
 
+const poppins = Poppins({
+  weight: ["500", "600", "700"],
+  subsets: ["latin"],
+});
+
 export default function Layout({ children }: PropsWithChildren) {
   return (
-    <div className="flex flex-col h-full">
+    <div className={cn("flex flex-col h-full", poppins.className)}>
       <Gnb />
-      <div className="flex-1 min-h-screen overflow-auto pt-40 pb-60">
-        {children}
-      </div>
+      <div className="flex-1 min-h-screen overflow-auto py-60">{children}</div>
       <BottomNavigation />
     </div>
   );
