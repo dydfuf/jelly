e17ad91f2424bda8a216b0ce8edc5ac24cc307ad
dydfuf <88dydfuf@naver.com>
2023-08-09 00:55:13 +0900

    feat: add pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,30 @@
 import withBundleAnalyzer from "@next/bundle-analyzer";
 import withPlugins from "next-compose-plugins";
+import withPWA from "next-pwa";
 import { env } from "./env.mjs";
 
 /**
  * @type {import('next').NextConfig}
  */
-const config = withPlugins([[withBundleAnalyzer({ enabled: env.ANALYZE })]], {
-  reactStrictMode: true,
-  rewrites() {
-    return [{ source: "/health", destination: "/api/health" }];
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "imagedelivery.net",
-      },
-    ],
-  },
-});
+const config = withPlugins(
+  [
+    [withBundleAnalyzer({ enabled: env.ANALYZE })],
+    [withPWA, { pwa: { dest: "public" } }],
+  ],
+  {
+    reactStrictMode: true,
+    rewrites() {
+      return [{ source: "/health", destination: "/api/health" }];
+    },
+    images: {
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "imagedelivery.net",
+        },
+      ],
+    },
+  }
+);
 
 export default config;
