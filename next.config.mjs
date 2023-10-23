6d3b495bd3acd65c6031342859063778f902711d
dydfuf <88dydfuf@naver.com>
2023-10-23 22:37:14 +0900

    sw 제거

--- next.config.mjs
+++ next.config.mjs
@@ -1,37 +1,23 @@
 import withBundleAnalyzer from "@next/bundle-analyzer";
 import withPlugins from "next-compose-plugins";
-import withPWA from "next-pwa";
 import { env } from "./env.mjs";
 
 /**
  * @type {import('next').NextConfig}
  */
-const config = withPlugins(
-  [
-    [withBundleAnalyzer({ enabled: env.ANALYZE })],
-    [
-      withPWA({
-        dest: "public",
-        customWorkerDir: "service-worker",
-        register: true,
-        skipWaiting: true,
-      }),
+const config = withPlugins([[withBundleAnalyzer({ enabled: env.ANALYZE })]], {
+  reactStrictMode: true,
+  rewrites() {
+    return [{ source: "/health", destination: "/api/health" }];
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "imagedelivery.net",
+      },
     ],
-  ],
-  {
-    reactStrictMode: true,
-    rewrites() {
-      return [{ source: "/health", destination: "/api/health" }];
-    },
-    images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "imagedelivery.net",
-        },
-      ],
-    },
-  }
-);
+  },
+});
 
 export default config;
