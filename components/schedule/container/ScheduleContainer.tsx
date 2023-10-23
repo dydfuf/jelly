da34489ca15f4fad22c37cf556a3e7b10f366c50
dydfuf <88dydfuf@naver.com>
2023-10-23 22:24:30 +0900

    sw reset

--- components/schedule/container/ScheduleContainer.tsx
+++ components/schedule/container/ScheduleContainer.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { env } from "env.mjs";
 import useGetSchedule from "hooks/schedule/useGetSchedule";
 import ScheduleCalendar from "../components/ScheduleCalendar";
 import ScheduleList from "../components/ScheduleList";
@@ -13,6 +14,8 @@ export default function ScheduleContainer() {
 
   return (
     <div className="flex flex-col pt-12 px-8 max-w-[640px] mx-auto">
+      <button onClick={sendNotification}>send</button>
+      <button onClick={subscribeUser}>subscribe</button>
       <ScheduleCalendar
         selectedDate={selectedDate}
         setSelectedDate={setSelectedDate}
@@ -23,4 +26,47 @@ export default function ScheduleContainer() {
       <ScheduleList schedules={schedules} selectedDate={selectedDate} />
     </div>
   );
+}
+
+async function sendNotification() {
+  try {
+    const response = await fetch("/api/send-notification", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error("Error sending notification:", error);
+  }
+}
+
+async function subscribeUser() {
+  console.log("subscript");
+  navigator.serviceWorker.ready.then((registration) => {
+    console.log("register");
+    registration.pushManager.getSubscription().then((subscription) => {
+      if (subscription) {
+        console.log("Already subscribed");
+      } else {
+        registration.pushManager
+          .subscribe({
+            userVisibleOnly: true,
+            applicationServerKey: process.env.WEB_PUSH_PUBLIC_KEY,
+          })
+          .then((subscription) => {
+            // save subscription on DB
+            fetch("/api/subscribe", {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify(subscription),
+            });
+          });
+      }
+    });
+  });
 }
