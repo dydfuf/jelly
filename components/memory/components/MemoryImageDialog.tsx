bee2e06280fb872808b99533073c0eee85e0a128
dydfuf <88dydfuf@naver.com>
2023-10-26 00:44:52 +0900

    이미지 슬라이더 구현.

--- components/memory/components/MemoryImageDialog.tsx
+++ components/memory/components/MemoryImageDialog.tsx
@@ -1,25 +1,55 @@
 import * as Dialog from "@radix-ui/react-dialog";
 import Image from "next/image";
+import { useEffect, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
 
 interface Props {
   open: boolean;
   setOpen: (open: boolean) => void;
   imageSrc: string;
+  imageSrcList: string[];
 }
 
-export default function MemoryImageDialog({ open, setOpen, imageSrc }: Props) {
+export default function MemoryImageDialog({
+  open,
+  setOpen,
+  imageSrc,
+  imageSrcList,
+}: Props) {
+  const findDefaultImageIdx = imageSrcList.findIndex(
+    (image) => image === imageSrc
+  );
+  const [currentIdx, setCurrentIdx] = useState(0);
+
+  useEffect(() => {
+    setCurrentIdx(findDefaultImageIdx + 1);
+  }, [findDefaultImageIdx, open]);
+
   return (
     <Dialog.Root open={open} onOpenChange={setOpen}>
-      <Dialog.Overlay className="fixed z-[9999] inset-0 data-[state=open]:bg-black/[0.3]" />
-      <Dialog.Content className="fixed z-[9999] left-1/2 top-1/2 max-w-[80dvw] w-[80dvw] max-h-[80dvh] h-[80vh] -translate-x-1/2 -translate-y-1/2 bg-transparent focus:outline-none">
-        <Dialog.Close asChild>
-          <Image
-            src={imageSrc}
-            fill
-            alt="image-dialog"
-            className="object-contain rounded-8"
-          />
-        </Dialog.Close>
+      <Dialog.Overlay className="fixed z-[9999] inset-0 data-[state=open]:bg-black/[0.7]" />
+      <Dialog.Content className="fixed z-[9999] left-1/2 top-1/2 w-full h-[-webkit-fill-available] -translate-x-1/2 -translate-y-1/2 bg-transparent focus:outline-none px-20">
+        <Swiper
+          className="w-full h-full"
+          initialSlide={findDefaultImageIdx}
+          pagination={{ type: "fraction" }}
+          onSlideChange={({ snapIndex }) => setCurrentIdx(snapIndex + 1)}
+        >
+          {imageSrcList.map((image) => (
+            <SwiperSlide key={image}>
+              <Image
+                src={image}
+                fill
+                alt="image-dialog"
+                className="object-contain"
+                priority
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+        <div className="flex justify-center mt-20">
+          <p className="bg-white px-20 rounded-full">{`${currentIdx} / ${imageSrcList.length}`}</p>
+        </div>
       </Dialog.Content>
     </Dialog.Root>
   );
