aef09e1736bf637298d652fd3ce8f61864da3516
dydfuf <88dydfuf@naver.com>
2023-10-31 01:09:00 +0900

    로그를 제거합니다.

--- pages/api/user/[userId]/group/[groupId]/memory.ts
+++ pages/api/user/[userId]/group/[groupId]/memory.ts
@@ -88,6 +88,40 @@ export default async function handler(
       },
     });
 
+    // send notification
+    // 1. 상대 유저의 userId를 가져옵니다.
+    const _userToGroup = await prisma.userToGroup.findMany({
+      where: { groupId: targetGruop.id },
+    });
+    const anotherUser = _userToGroup.filter(
+      (userToGroup) => userToGroup.userId !== userId
+    );
+    // 2. 상대 유저의 userId를 가진 subscript을 가져옵니다.
+    if (anotherUser) {
+      const pushSubscription = await prisma.pushSubscription.findMany({
+        where: {
+          userId: anotherUser[0].userId,
+        },
+      });
+
+      // 3. 해당 정보를 통해 sendnotification을 호출합니다.
+      for (const subs of pushSubscription) {
+        await sendNotification({
+          subscription: JSON.parse(
+            subs.subscription
+          ) as unknown as webpush.PushSubscription,
+          options: {
+            title: "내사랑 젤리가 젤리에 글을 등록했어요! 🪼",
+            body: `${format(
+              new Date(date),
+              "MM월 dd일"
+            )}추억을 등록했어요!\n확인하러 가볼까요~?❤️`,
+            url: `/memory/${format(new Date(date), "yyyy-MM-dd")}`,
+          },
+        });
+      }
+    }
+
     return res.status(200).json(memroy);
   }
 
@@ -132,8 +166,6 @@ export default async function handler(
       (userToGroup) => userToGroup.userId !== userId
     );
 
-    console.log({ _userToGroup, anotherUser });
-
     // 2. 상대 유저의 userId를 가진 subscript을 가져옵니다.
     if (anotherUser) {
       const pushSubscription = await prisma.pushSubscription.findMany({
@@ -149,11 +181,11 @@ export default async function handler(
             subs.subscription
           ) as unknown as webpush.PushSubscription,
           options: {
-            title: "내사랑 젤리가 젤리에 글을 등록했어요! 🪼",
+            title: "내사랑 젤리가 젤리에 글을 수정했어요! 🪼",
             body: `${format(
               new Date(date),
               "MM월 dd일"
-            )}추억을 등록했어요!\n확인하러 가볼까요~?❤️`,
+            )}추억을 수정했어요!\n확인하러 가볼까요~?❤️`,
             url: `/memory/${format(new Date(date), "yyyy-MM-dd")}`,
           },
         });
